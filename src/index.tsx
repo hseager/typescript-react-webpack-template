7d0b01eeb5986bdd404f9dae08d15210a47b55da
hseager <harvey.seager@googlemail.com>
2022-01-12 20:40:26 +0000

    Added prettier and fixed TS errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 export const App: React.FC = () => {
-    return (<div>Hello World</div>)
+  return <div>Hello World</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -6,45 +6,42 @@ const srcDir = path.resolve(__dirname, 'src')
 const outputDir = path.resolve(__dirname, 'dist')
 
 module.exports = {
-    devtool: 'source-map',
-    entry: path.join(srcDir, 'index.tsx'),
-    output: {
-        path: outputDir,
-        filename: 'js/[name].bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-    },
-    plugins: [
-        new RemovePlugin({
-            before: {
-                log: false,
-                include: [
-                    path.join(outputDir, 'js')
-                ]
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(srcDir, 'index.html')
-        })
+  devtool: 'source-map',
+  entry: path.join(srcDir, 'index.tsx'),
+  output: {
+    path: outputDir,
+    filename: 'js/[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new RemovePlugin({
+      before: {
+        log: false,
+        include: [path.join(outputDir, 'js')],
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(srcDir, 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.tsx?$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: [/\.js$/, /\.tsx?$/],
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-typescript',
-                            ['@babel/preset-react', { runtime: "automatic" }]
-                        ]
-                    }
-                }
-            }
-        ]
-    }
+  },
 }
-
